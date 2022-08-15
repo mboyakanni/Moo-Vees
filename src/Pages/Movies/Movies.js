aae0ec7fe880ec7d9065e5e741abe1c45aaf99a3
Raheem Mubarak <raheemmubarak7@gmail.com>
2022-08-15 23:44:27 +0100

    Update

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Single from "../../components/Single/Single";
+import CustomPagination from "../../components/Pagination/CustomPagination";
+import Genres from "../../components/Genres";
+import useGenre from "../../hooks/useGenre";
 
 const Movies = () => {
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  const [numOfPages, setNumOfPages] = useState();
+  const [genres, setGenres] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const genreforURL = useGenre(selectedGenres);
+  const fetchMovies = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
+    );
+    setContent(data.results);
+    setNumOfPages(data.total_pages);
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, [page, genreforURL]);
   return (
-    <div>Movies</div>
-  )
-}
+    <div>
+      <span className="pageTitle">Movies</span>
+      <Genres
+        type="movie"
+        selectedGenres={selectedGenres}
+        setSelectedGenres={setSelectedGenres}
+        genres={genres}
+        setGenres={setGenres}
+        setPage={setPage}
+      />
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <Single
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type="movie"
+              vote_average={c.vote_average}
+            />
+          ))}
+      </div>
+      <div>
+        {numOfPages > 1 && (
+          <CustomPagination setPage={setPage} numOfPages={numOfPages} />
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Movies
+export default Movies;
