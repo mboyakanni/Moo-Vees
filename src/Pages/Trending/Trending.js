7e481ec8666204cfa5951c904e6c5c5891e042d8
Raheem Mubarak <raheemmubarak7@gmail.com>
2022-08-15 23:45:33 +0100

    U

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -22,7 +22,7 @@ const Trending = () => {
 
   return (
     <div>
-      <span className="pageTitle">Trending Today</span>
+      <span className="pageTitle">Trending Today</span
       <div className="trending">
         {content &&
           content.map((c) => (
